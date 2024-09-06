dd3502408d7e1e24d020c85304c2be23453829ac
Cornel Stoica <cornel.stoica@oit.edu>
2024-09-06 02:48:52 -0700

    Added dotenv config to client app to be available before components building.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Load environment variables from .env file
+import dotenv from "dotenv";
+dotenv.config();
+
 createRoot(document.getElementById("root")).render(<App />);
